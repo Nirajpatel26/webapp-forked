520ac8fe9b78d48de8ac5b85028a42eee9e10f48
NIRAJ <patel.niraju@northeastern.edu>
2025-04-12 00:59:09 -0400

    removing cicd endpoint

--- app.js
+++ app.js
@@ -76,53 +76,6 @@ app.all("/healthz", setHeaders ,async (req, res) => {
     }
 });
 
-
-app.all("/cicd", setHeaders ,async (req, res) => {
-    const startTime = new Date();
-    try {
-
-        statsd.increment('api.healthz');
-
-        if (req.method !== 'GET') {
-            
-            logger.warn(`Health check failed: Method ${req.method} not allowed`);
-            statsd.increment('api.healthz.method_not_allowed');
-            return res.status(405).send();
-        }
-
-
-        if (Object.keys(req.body).length > 0 || Object.keys(req.query).length > 0 || req.get("Content-Length")!== undefined || req.get("Authorization") ||
-        req.get("authentication")  ) {
-            
-            logger.warn('Health check failed: Payload should be empty');
-            statsd.increment('api.healthz.bad_request');
-            return res.status(400).send();
-        }
-       
-        const dbStartTime = new Date();
-        await HealthCheck.create({
-            datetime: new Date()
-        });
-        const dbDuration = new Date() - dbStartTime;
-        statsd.timing('db.healthcheck.create.time', dbDuration);
-        logger.info(`Health check record created`);
-        
-        logger.info(`GET API /v1/file: Created file record` );
-        statsd.increment('api.healthz.success');
-
-        res.status(200).send();
-    
-    } catch (error) {
-        
-        logger.error('Health check error',error);
-        statsd.increment('api.healthz.error');
-        res.status(503).send();
-    }finally {
-        const duration = new Date() - startTime;
-        statsd.timing('api.healthz.time', duration);
-    }
-});
-
 app.use(setHeaders);
 
 app.use('/',file_route);
